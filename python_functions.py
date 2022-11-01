ab70179283e6db1e12859ff21d41e083dc6cafbd
Ryan Combest <crcombest@gmail.com>
2022-11-01 16:46:37 -0500

    completes exercise 2

--- python_functions.py
+++ python_functions.py
@@ -1,8 +1,19 @@
-def sum_to(num):
-  sum = 0
-  for n in range(1, num + 1):
-    sum += n
-  return sum
+# EXERCISE 1
+
+# def sum_to(num):
+#   sum = 0
+#   for n in range(1, num + 1):
+#     sum += n
+#   return sum
+# print(sum_to(6))
 
 
-print(sum_to(6))
+#EXERCISE 2
+
+def largest(nums):
+  largest = 0
+  for num in nums:
+    if num > largest:
+      largest = num
+  return largest
+print(largest([10, 15, 96, 85, 3]))
