bad9ad6c9b8675a91446d93456a0f82a986d39ff
Ryan Combest <crcombest@gmail.com>
2022-11-01 17:37:34 -0500

    completes exercise 4

--- python_functions.py
+++ python_functions.py
@@ -21,6 +21,15 @@
 
 #EXERCISE 3
 
-def occurances(str1, str2):
-  return str1.count(str2)
-print(occurances('fleep floop', 'p'))
+# def occurances(str1, str2):
+#   return str1.count(str2)
+# print(occurances('fleep floop', 'p'))
+
+# EXERCISE 4
+
+def product(*args):
+  product = 1
+  for arg in args:
+    product *= arg
+  return product
+print(product(2, 5, 5))
