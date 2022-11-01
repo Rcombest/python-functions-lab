9ecea2de4469bbe6483cf9b1218174011bb02a00
Ryan Combest <crcombest@gmail.com>
2022-11-01 17:33:32 -0500

    completes exercise 3

--- python_functions.py
+++ python_functions.py
@@ -10,10 +10,17 @@
 
 #EXERCISE 2
 
-def largest(nums):
-  largest = 0
-  for num in nums:
-    if num > largest:
-      largest = num
-  return largest
-print(largest([10, 15, 96, 85, 3]))
+# def largest(nums):
+#   largest = 0
+#   for num in nums:
+#     if num > largest:
+#       largest = num
+#   return largest
+# print(largest([10, 15, 96, 85, 3]))
+
+
+#EXERCISE 3
+
+def occurances(str1, str2):
+  return str1.count(str2)
+print(occurances('fleep floop', 'p'))
